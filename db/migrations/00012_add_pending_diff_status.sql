8ea81b5dffa9f331e3e55e45929000747a47e810
Eric Smith <paytonrules@gmail.com>
2021-01-10 20:25:06 -0600

    Alter the type in place - use No transaction
    Using no-transaction on the migration allows using the alter in place.

--- db/migrations/00012_add_pending_diff_status.sql
+++ db/migrations/00012_add_pending_diff_status.sql
@@ -1,27 +1,6 @@
+-- +goose NO TRANSACTION
 -- +goose Up
-DROP INDEX storage_diff_new_status_index;
-DROP INDEX storage_diff_unrecognized_status_index;
-
-ALTER TABLE public.storage_diff ALTER COLUMN status DROP DEFAULT;
-ALTER TABLE public.storage_diff ALTER COLUMN status TYPE VARCHAR(255);
-
-DROP TYPE public.diff_status;
-CREATE TYPE public.diff_status AS ENUM (
-    'new',
-    'pending',
-    'transformed',
-    'unrecognized',
-    'noncanonical',
-    'unwatched'
-    );
-
-ALTER TABLE public.storage_diff ALTER COLUMN status TYPE public.diff_status USING (status::diff_status);
-ALTER TABLE public.storage_diff ALTER COLUMN status SET DEFAULT 'new';
-
-CREATE INDEX storage_diff_new_status_index
-    ON public.storage_diff (status) WHERE status = 'new';
-CREATE INDEX storage_diff_unrecognized_status_index
-    ON public.storage_diff (status) WHERE status = 'unrecognized';
+ALTER TYPE public.diff_status ADD VALUE 'pending' AFTER 'new';
 
 CREATE INDEX storage_diff_pending_status_index
     ON public.storage_diff (status) WHERE status = 'pending';
